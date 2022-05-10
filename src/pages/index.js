710d3c2d54196268eb86ef5b19462e98dfa091e4
Tomek <dsd@wo>
2022-05-10 16:16:53 +0200

    feat add navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "../styles/index.scss";
+import facebookIcon from "../images/facebook.svg";
+import snapIcon from "../images/snap.svg";
+
 import Slider from "./Component/Slider";
 
 // styles
@@ -9,11 +12,32 @@ import Slider from "./Component/Slider";
 // markup
 const IndexPage = () => {
   return (
-    //navbar
-    // section hero
     <>
+      {/* navbar */}
+      <div className="navbar">
+        <div className="navbar-logo font-48px font-bold">LOGO</div>
+        <div className="navbar-list font-16px">
+          <ul>
+            <li>Poznaj przestrzeń</li>
+            <li>Oferta</li>
+            <li>Lokalizacja</li>
+            <li>Własne biuro</li>
+            <li>Kontakt</li>
+          </ul>
+          <div className="navbar-social">
+            <img src={facebookIcon} alt="Facebook logo" />
+            <img
+              style={{ marginLeft: "23px" }}
+              src={snapIcon}
+              alt="Snap logo"
+            />
+          </div>
+        </div>
+      </div>
+      {/* end navbar */}
+      {/* section hero */}
       <Slider />
-
+      {/* end section hero */}
       <div>testowu wpis</div>
     </>
   );
