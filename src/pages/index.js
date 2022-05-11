66861857e5c03bbae1b96963d56fba2406b7639e
Tomek <dsd@wo>
2022-05-11 18:01:01 +0200

    fix image in advertisement

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,97 +18,99 @@ import Slider from "./Component/Slider";
 const IndexPage = () => {
   return (
     <>
-      {/* navbar */}
-      <div className="navbar">
-        <div className="navbar-logo font-48px font-bold">LOGO</div>
-        <div className="navbar-list font-16px">
-          <ul>
-            <li>Poznaj przestrzeń</li>
-            <li>Oferta</li>
-            <li>Lokalizacja</li>
-            <li>Własne biuro</li>
-            <li>Kontakt</li>
-          </ul>
-          <div className="navbar-social">
-            <img src={facebookIcon} alt="Facebook logo" />
-            <img
-              style={{ marginLeft: "23px" }}
-              src={snapIcon}
-              alt="Snap logo"
-            />
-          </div>
-        </div>
-      </div>
-      {/* end navbar */}
-      {/* section hero */}
-      <Slider />
-      {/* end section hero */}
-      {/* section advertisement */}
-      <div className="advertisement">
-        <div className="advertisement-up-line">
-          <div className="advertisement-up-line-image-container">
-            <img alt="Bład zdjęcia" src={advertisementImage1}></img>
-            <div className="text">
-              <div className="square"></div>
-              <p className="font-28px font-bold name">Biuro</p>
-              <div className="line"></div>
-              <p className="price font-18px">od 1000zł/msc</p>
-            </div>
-            <div className="arrow-in-image">
-              <img src={arrowNext}></img>
-            </div>
-          </div>
-          <div
-            className="advertisement-up-line-image-container"
-            style={{ marginLeft: "30px" }}
-          >
-            <img alt="Bład zdjęcia" src={advertisementImage2}></img>
-            <div className="text">
-              <div className="square"></div>
-              <p className="font-28px font-bold name">
-                Lorem ipsum <br></br>Biuro
-              </p>
-              <div className="line"></div>
-              <p className="price font-18px">od 500zł/msc</p>
-            </div>
-            <div className="arrow-in-image">
-              <img src={arrowNext}></img>
+      <body>
+        {/* navbar */}
+        <div className="navbar">
+          <div className="navbar-logo font-48px font-bold">LOGO</div>
+          <div className="navbar-list font-16px">
+            <ul>
+              <li>Poznaj przestrzeń</li>
+              <li>Oferta</li>
+              <li>Lokalizacja</li>
+              <li>Własne biuro</li>
+              <li>Kontakt</li>
+            </ul>
+            <div className="navbar-social">
+              <img src={facebookIcon} alt="Facebook logo" />
+              <img
+                style={{ marginLeft: "23px" }}
+                src={snapIcon}
+                alt="Snap logo"
+              />
             </div>
           </div>
         </div>
-        <div className="advertisement-down-line">
-          <div className="advertisement-down-line-image-container">
-            <img alt="Bład zdjęcia" src={advertisementImage3}></img>
-            <div className="text">
-              <div className="square"></div>
-              <p className="font-28px font-bold name">Biuro lorem</p>
-              <div className="line"></div>
-              <p className="price font-18px">od 100zł/h</p>
+        {/* end navbar */}
+        {/* section hero */}
+        <Slider />
+        {/* end section hero */}
+        {/* section advertisement */}
+        <div className="advertisement">
+          <div className="advertisement-up-line">
+            <div className="advertisement-up-line-image-container">
+              <img alt="Bład zdjęcia" src={advertisementImage1}></img>
+              <div className="text">
+                <div className="square"></div>
+                <p className="font-28px font-bold name">Biuro</p>
+                <div className="line"></div>
+                <p className="price font-18px">od 1000zł/msc</p>
+              </div>
+              <div className="arrow-in-image">
+                <img src={arrowNext} alt="przejdz"></img>
+              </div>
             </div>
-            <div className="arrow-in-image">
-              <img src={arrowNext}></img>
+            <div
+              className="advertisement-up-line-image-container"
+              style={{ marginLeft: "30px" }}
+            >
+              <img alt="Bład zdjęcia" src={advertisementImage2}></img>
+              <div className="text">
+                <div className="square"></div>
+                <p className="font-28px font-bold name">
+                  Lorem ipsum <br></br>Biuro
+                </p>
+                <div className="line"></div>
+                <p className="price font-18px">od 500zł/msc</p>
+              </div>
+              <div className="arrow-in-image">
+                <img src={arrowNext} alt="przejdz"></img>
+              </div>
             </div>
           </div>
-          <div
-            className="advertisement-down-line-image-container"
-            style={{ marginLeft: "30px" }}
-          >
-            <img alt="Bład zdjęcia" src={advertisementImage4}></img>
-            <div className="text">
-              <div className="square"></div>
-              <p className="font-28px font-bold name">
-                Biuro lorem ipsum <br></br>Lorem
-              </p>
-              <div className="line line-next"></div>
-              <p className="price font-18px">od 100zł/h</p>
+          <div className="advertisement-down-line">
+            <div className="advertisement-down-line-image-container">
+              <img alt="Bład zdjęcia" src={advertisementImage3}></img>
+              <div className="text">
+                <div className="square"></div>
+                <p className="font-28px font-bold name">Biuro lorem</p>
+                <div className="line"></div>
+                <p className="price font-18px">od 100zł/h</p>
+              </div>
+              <div className="arrow-in-image">
+                <img src={arrowNext} alt="przejdz"></img>
+              </div>
             </div>
-            <div className="arrow-in-image">
-              <img src={arrowNext}></img>
+            <div
+              className="advertisement-down-line-image-container"
+              style={{ marginLeft: "30px" }}
+            >
+              <img alt="Bład zdjęcia" src={advertisementImage4}></img>
+              <div className="text">
+                <div className="square"></div>
+                <p className="font-28px font-bold name">
+                  Biuro lorem ipsum <br></br>Lorem
+                </p>
+                <div className="line line-next"></div>
+                <p className="price font-18px">od 100zł/h</p>
+              </div>
+              <div className="arrow-in-image">
+                <img src={arrowNext} alt="przejdz"></img>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* end section advertisement */}
+        {/* end section advertisement */}
+      </body>
     </>
   );
 };
